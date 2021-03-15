6efc2a96ee93576dd6e33c70933a725bee4a838a
Md. Mojnu Miah <mojnu.cse.pust@gmail.com>
2021-03-15 13:03:38 +0600

    40-5-2

--- src/App.js
+++ src/App.js
@@ -15,27 +15,45 @@ import {
 import Home from './components/Home/Home';
 import NoMatch from './components/NoMatch/NoMatch';
 import FriendDetail from './components/FriendDetail/FriendDetail';
+import AccessAlarmIcon from '@material-ui/icons/AccessAlarm';
+import ThumbUpIcon from '@material-ui/icons/ThumbUp';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 
 function App() {
-  
+  const [color, setColor] = useState('');
+  const[users, setUsers] = useState([]);
+  const[singleUser, setSingleUser] = useState([]);
+  const[randomUsers, setRandomUsers] = useState({});
+  const handleColor = () => {
+    let thumbColor = color ? '' : 'primary';
+    setColor(thumbColor);
+  }
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users/')
+    .then(res => res.json())
+    .then(data => setUsers(data))
+    //singleUser
+    fetch('https://jsonplaceholder.typicode.com/users/9')
+    .then(res => res.json())
+    .then(data => setSingleUser(data))
+    //randomUser
+    fetch('https://randomuser.me/api')
+    .then(res => res.json())
+    .then(data => setRandomUsers(data.results[0]))
+  }, [])
+  console.log(randomUsers)
   return (
-    <Router>
-      <Switch>
-        <Route path="/home">
-          <Home></Home>
-        </Route>
-        <Route path="/about/:friendId">
-          <FriendDetail></FriendDetail>
-        </Route>
-        <Route exact path="/">
-            <Home></Home>
-        </Route>
-        <Route path="*">
-          <NoMatch></NoMatch>
-        </Route>
-      </Switch>
-    </Router>
+    <div>
+      <h1>{singleUser.name}</h1>
+      <h2>{randomUsers.login?.password}</h2>
+      <AccessAlarmIcon/>
+      <ThumbUpIcon onClick={handleColor} color={color}></ThumbUpIcon>
+      {
+        users.map(user => <li>{user.name}</li>)
+      }
+    </div>
   );
 }
 
